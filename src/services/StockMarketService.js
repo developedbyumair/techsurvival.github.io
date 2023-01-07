e50f1c27dd5a296ecbe337cbc6fc94582d79c9d2
Umair-Ali-01 <umairsadaqat@invotyx.co.uk>
2023-01-07 14:40:21 +0500

    delete react template for redirection deployment

--- src/services/StockMarketService.js
+++
@@ -1,8 +0,0 @@
-
-function getMarketData(marketSymbol) {
-    // fetching stockmarket data from proxy server hosted on https://newsylandstockmarketindexserver.onrender.com
-    const url = 'https://newsylandstockmarketindexserver.onrender.com/stockMarketData/' + marketSymbol;
-    return fetch(url).then((res) => res.json());
-  };
-
-export default getMarketData;
